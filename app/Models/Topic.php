0ecfd7cd49bd7022d3046255bca6b6da92934eb3
nb3n <hello@nben.com.np>
2025-11-05 08:25:04 +0000

    Topic Model & Migration

---
+++ app/Models/Topic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'cover',
+        'content',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
