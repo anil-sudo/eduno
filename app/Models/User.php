2d18cd025e6431a69c5adb8b23ba2ee88cf3b6da
nb3n <hello@nben.com.np>
2025-11-05 07:54:18 +0000

    hasAvatar trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+// Filamnet
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasAvatar;
 use Filament\Panel;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable implements FilamentUser, HasAvatar
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
@@ -63,5 +65,22 @@ class User extends Authenticatable implements FilamentUser
         ];
 
         return in_array($this->email, $allowedEmails);
+    }
+
+    public function getFilamentAvatarUrl(): ?string
+    {
+        // If user has uploaded an avatar, return its CDN URL
+        // if (!empty($this->avatar)) {
+        //     return getCdnUrl($this->avatar);
+        // }
+
+        // Otherwise, generate a fallback avatar using Boring Avatars
+        $name = str($this->name)
+            ->trim()
+            ->explode(' ')
+            ->map(fn (string $segment): string => filled($segment) ? mb_substr($segment, 0, 1) : '')
+            ->join(' ');
+
+        return 'https://boring-avatars-api.vercel.app/api/avatar?size=40&variant=beam&name=' . urlencode($name);
     }
 }
